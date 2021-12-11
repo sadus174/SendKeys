8a996e1464c55db2fd686676dafb87a563c7bf7d
sadus174 <34853375+sadus174@users.noreply.github.com>
2021-12-11 09:30:42 +0500

    Исправлены ошибки. Форма стала компактней. Добавлен функционал выбора клавишь для симуляции

--- send_keys/MainForm.Designer.cs
+++ send_keys/MainForm.Designer.cs
@@ -33,50 +33,258 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressbar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // timer2_command
+            // 
+            this.timer2_command.Tick += new System.EventHandler(this.timer2_command_Tick);
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(15, 57);
+            this.comboBox1.Location = new System.Drawing.Point(158, 11);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(405, 21);
+            this.comboBox1.Size = new System.Drawing.Size(207, 21);
             this.comboBox1.TabIndex = 10;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 41);
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(13, 13);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(129, 13);
+            this.label4.Size = new System.Drawing.Size(139, 15);
             this.label4.TabIndex = 11;
             this.label4.Text = "Выберите целевое окно";
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(-1, -1);
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(388, 13);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(188, 39);
+            this.button4.Size = new System.Drawing.Size(188, 25);
             this.button4.TabIndex = 19;
             this.button4.Text = "Обновить страницу";
             this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // Form1
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(15, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 15);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Выберите клавиши симуляции";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(193, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 15);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Текущие клавиши симуляции";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(13, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(221, 15);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Пауза между нажатиями внутри цикла";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(240, 246);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 20);
+            this.textBox1.TabIndex = 26;
+            this.textBox1.Text = "3";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(149, 272);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(216, 20);
+            this.textBox2.TabIndex = 28;
+            this.textBox2.Text = "10";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(13, 274);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(130, 15);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "Пауза между циклами";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(196, 336);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(169, 25);
+            this.button3.TabIndex = 30;
+            this.button3.Text = "Запуск";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 336);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 25);
+            this.button2.TabIndex = 31;
+            this.button2.Text = "Стоп";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressbar1
+            // 
+            this.progressbar1.Location = new System.Drawing.Point(18, 367);
+            this.progressbar1.Name = "progressbar1";
+            this.progressbar1.Size = new System.Drawing.Size(347, 25);
+            this.progressbar1.TabIndex = 32;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(251, 302);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(114, 20);
+            this.textBox3.TabIndex = 34;
+            this.textBox3.Text = "10";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(13, 304);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(232, 15);
+            this.label6.TabIndex = 33;
+            this.label6.Text = "Количество итераций (0 - неограничено)";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 398);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(583, 22);
+            this.statusStrip1.TabIndex = 35;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // listBox3
+            // 
+            this.listBox3.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 14;
+            this.listBox3.Location = new System.Drawing.Point(388, 63);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(176, 186);
+            this.listBox3.TabIndex = 36;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 14;
+            this.listBox1.Items.AddRange(new object[] {
+            "{DELETE}",
+            "{DOWN}",
+            "{UP}",
+            "{LEFT}",
+            "{RIGHT}",
+            "{1}",
+            "{2}",
+            "{Esc}",
+            "{ENTER}",
+            "-ЛКМ-",
+            "-ПКМ-"});
+            this.listBox1.Location = new System.Drawing.Point(16, 59);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(174, 172);
+            this.listBox1.TabIndex = 20;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 14;
+            this.listBox2.Location = new System.Drawing.Point(196, 59);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(169, 172);
+            this.listBox2.TabIndex = 22;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(385, 41);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(92, 15);
+            this.label7.TabIndex = 37;
+            this.label7.Text = "Лог симуляции";
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(187, 35);
-            this.ControlBox = false;
+            this.ClientSize = new System.Drawing.Size(583, 420);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.progressbar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.comboBox1);
-            this.Name = "Form1";
-            this.ShowIcon = false;
-            this.ShowInTaskbar = false;
+            this.Name = "MainForm";
             this.Text = "Симулятор нажатия клавиш";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -87,6 +295,23 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressbar1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- send_keys/MainForm.cs
+++ send_keys/MainForm.cs
@@ -30,6 +30,8 @@ namespace send_keys
         public int count_pos;
         IntPtr windows_Handle;
         string name_window;
+        int timer1;
+        int timer2;
 
         //Использование в качестве разделителя целой и дробной части , и .
         public int set_dec_separation (string value)
@@ -44,15 +46,14 @@ namespace send_keys
             TopMost = true;
             InitializeComponent();
         }
-        
-   
-   
-        
+         
         //Загрузка формы. Установление статуса и полей значениями по умолчанию. Так же получение списка открытых окон
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.FormBorderStyle = FormBorderStyle.None;
-            this.Location = new Point(0, 980);
+            toolStripStatusLabel1.Text = ("Программа не активна");
+
+            //this.FormBorderStyle = FormBorderStyle.None;
+            //this.Location = new Point(0, 980);
 
             Process[] processlist = Process.GetProcesses();
 
@@ -68,43 +69,119 @@ namespace send_keys
             int index = comboBox1.FindString("Портал муниципальной");
             comboBox1.SelectedIndex = index;
         }
-        
-        //Закрытие формы
-        private void button3_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-        
+               
         //Выбор целевого окна для симуляции
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             name_window = comboBox1.SelectedItem.ToString();
         }
-        
 
-
-
-        private void button4_Click(object sender, EventArgs e)
+        //Выбор клавишь из списка для симуляции
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            name_window = comboBox1.SelectedItem.ToString();
+            string selected_keys = listBox1.SelectedItem.ToString();
+            listBox2.Items.Add(selected_keys);
+        }
+        //Выбор клавишь из списка для симуляции
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string selected_keys = listBox1.SelectedItem.ToString();
+            listBox2.Items.Add(selected_keys);
+        }
+
+        //Запуск симуляции
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            //Задержка между отправками клавиш
+            timer1 = set_dec_separation(textBox1.Text);
+            //Задержка между командами
+            timer2 = set_dec_separation(textBox2.Text);
+            //Установка величины задержки между отправками команды (в миллисекундах)
+            //MessageBox.Show("Сработка таймера через "+timer2.ToString()+" миллесекунд.");
+            timer2_command.Interval = timer2;
+            //Количество посещений для снятия
+            count_pos = Convert.ToInt32(textBox3.Text);
+            //ProgressBar
+            progressbar1.Value = 0;
+            progressbar1.Maximum = count_pos;
+            // Получение handle окна по его имени из combobox
             windows_Handle = FindWindow(null, name_window);
             // Проверка на существование окна
             if (windows_Handle == IntPtr.Zero)
             {
+                toolStripStatusLabel1.Text = "Приложение " + name_window + " не найдено, выполнение остановлено.";
                 return;
             }
             else
             {
+                toolStripStatusLabel1.Text = "Приложение " + name_window + " найдено, Начато выполнение.";
                 //MessageBox.Show("Handle выбранного окна " + windows_Handle.ToString());
                 SetForegroundWindow(windows_Handle);
-                SendKeys.SendWait("{F5}");
+                //Запуск таймера
+                button3.Enabled = false;
+                timer2_command.Enabled = true;
             }
+        }
 
-            
+        //Метод для симуляции нажатия клавишь и формирования задержки между тактами и циклами
+        private void timer2_command_Tick(object sender, EventArgs e)
+        {
+            //Перебор списка симуляционных клавиш
+            for (int i = 0; i < this.listBox2.Items.Count; i++)
+            {
+                if ((this.listBox2.Items[i].ToString() == "-ЛКМ-") || (this.listBox2.Items[i].ToString() == "-ПКМ-"))
+                {
+                    SendKeys.Send("Должна быть нажата какая кнопка мыши");
+                    Thread.Sleep(Convert.ToInt32(timer1));
 
+                }
+                else
+                {
+                    SendKeys.Send(this.listBox2.Items[i].ToString());
+                    this.listBox3.Items.Add(this.listBox2.Items[i].ToString());  
+                }
+                //если команда последняя, то не выполнять задержку потока
+                if (i != this.listBox2.Items.Count - 1)
+                {
+                    //Задержка выполнения потока
+                    //listBox3.Items.Add("Нажата клавиша" + this.listBox2.Items[i].ToString());
+                    Thread.Sleep(Convert.ToInt32(timer1));
+                }
 
+            }
+            //Количество оставшихся итераций
+            count_pos = count_pos - 1;
+            //Выполнение кода определённое количество раз, в случае превышения количества - остановка таймера
+            if (count_pos != 0)
+            {
+                toolStripStatusLabel1.Text = "Программа активена. Осталось итераций: " + Convert.ToString(count_pos);
+                progressbar1.Value = progressbar1.Maximum - count_pos;
+            }
+            else
+            {
+                timer2_command.Enabled = false;
+                button3.Enabled = true;
+                toolStripStatusLabel1.Text = ("Программа не активна");
+                progressbar1.Value = progressbar1.Maximum;
+            }
+        }
 
+        //Остановка таймера
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer2_command.Enabled = false;
+            button3.Enabled = true;
+            toolStripStatusLabel1.Text = ("Программа не активна");
+            progressbar1.Value = 0;
+        }
 
+        //Удаление клавишь из назначения симуляции
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedIndex != -1)
+                listBox2.Items.RemoveAt(listBox2.SelectedIndex);
+            else
+                toolStripStatusLabel1.Text = ("Выберите элемент");
         }
     }
 }
