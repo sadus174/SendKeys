a9bedd763a402d4db63cf2731847a74edee05dd8
sadus174 <34853375+sadus174@users.noreply.github.com>
2021-12-11 08:12:46 +0500

    Добавьте файлы проекта.

---
+++ send_keys/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace send_keys
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer2_command = new System.Windows.Forms.Timer(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(15, 57);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(405, 21);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Выберите целевое окно";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(-1, -1);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(188, 39);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Обновить страницу";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(187, 35);
+            this.ControlBox = false;
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Симулятор нажатия клавиш";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer2_command;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ send_keys/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+
+namespace send_keys
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool BringWindowToTop(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        public int count_pos;
+        IntPtr windows_Handle;
+        string name_window;
+
+        //Использование в качестве разделителя целой и дробной части , и .
+        public int set_dec_separation (string value)
+        {         
+            float millisecond = Single.Parse(value.Replace(",", "."), CultureInfo.InvariantCulture) * 1000;
+            return Convert.ToInt32(millisecond);
+        }
+
+        //Инициализация формы и ее объектов
+        public Form1()
+        {
+            TopMost = true;
+            InitializeComponent();
+        }
+        
+   
+   
+        
+        //Загрузка формы. Установление статуса и полей значениями по умолчанию. Так же получение списка открытых окон
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Location = new Point(0, 980);
+
+            Process[] processlist = Process.GetProcesses();
+
+            //Получение списка открытых окон и наполнение combobox'a
+            foreach (Process process in processlist)
+            {
+                if (!String.IsNullOrEmpty(process.MainWindowTitle))
+                {
+                    comboBox1.Items.Add(Convert.ToString(process.MainWindowTitle));
+                }
+            }
+
+            int index = comboBox1.FindString("Портал муниципальной");
+            comboBox1.SelectedIndex = index;
+        }
+        
+        //Закрытие формы
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
+        //Выбор целевого окна для симуляции
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            name_window = comboBox1.SelectedItem.ToString();
+        }
+        
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            name_window = comboBox1.SelectedItem.ToString();
+            windows_Handle = FindWindow(null, name_window);
+            // Проверка на существование окна
+            if (windows_Handle == IntPtr.Zero)
+            {
+                return;
+            }
+            else
+            {
+                //MessageBox.Show("Handle выбранного окна " + windows_Handle.ToString());
+                SetForegroundWindow(windows_Handle);
+                SendKeys.SendWait("{F5}");
+            }
+
+            
+
+
+
+
+        }
+    }
+}
